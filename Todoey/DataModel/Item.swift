bf362611589e6fd7e465d1912ec83de194539b8f
Holmes-EH <holmes.samuel@yahoo.fr>
2018-12-09 17:56:19 +0100

    Convert to MVC Design Pattern

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,16 @@
+//
+//  ItemClass.swift
+//  Todoey
+//
+//  Created by Samuel Holmes on 09/12/2018.
+//  Copyright © 2018 Samuel Holmes. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+
+        var title : String = ""
+        var done : Bool = false
+
+}
